9d651522f39d1e1116406cd2e177573a5e54001c
Asykur <asykur@sehati.co>
2019-11-30 15:10:58 +0700

    Initial Commit

---
+++ Ovo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ovo
+//
+//  Created by asykur khamid on 30/11/19.
+//  Copyright © 2019 asykur khamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
